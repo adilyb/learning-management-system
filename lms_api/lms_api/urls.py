96f77ec1198760fc780bedfc95cbf19a446994f2
adilyb <muhammedadilyb@gmail.com>
2024-03-27 01:59:58 +0530

    DRF Authentication

--- lms_api/lms_api/urls.py
+++ lms_api/lms_api/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('api/', include('main.urls')),
+    path('rest-auth/', include('rest_framework.urls')),
+    
 ]
--- lms_api/main/views.py
+++ lms_api/main/views.py
@@ -4,12 +4,14 @@ from rest_framework import generics
 from .serializers import TeacherSerializers
 from .models import *
 from rest_framework.response import Response
+from rest_framework import permissions
 # Create your views here.
 
 class TeacherList(generics.ListCreateAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializers
-
+    permission_classes = [permissions.IsAuthenticated]
+    
 class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializers
