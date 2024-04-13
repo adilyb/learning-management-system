4d3713dc2fbf8c8b08f3b6a905d8863ed692e044
adilyb <muhammedadilyb@gmail.com>
2024-04-13 23:35:06 +0530

    teacher login post views.py

--- lms_api/main/views.py
+++ lms_api/main/views.py
@@ -5,6 +5,7 @@ from .serializers import TeacherSerializers
 from .models import *
 from rest_framework.response import Response
 from rest_framework import permissions
+from django.http import JsonResponse
 # Create your views here.
 
 class TeacherList(generics.ListCreateAPIView):
@@ -16,4 +17,13 @@ class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializers
     
+def teacher_login(request):
+    email = request.POST['email']
+    password = request.POST['password']
+    teacherData = models.Teacher.objects.get(email=email, password=password)
     
+    if teacherData:
+        return JsonResponse ({'bool':True})
+    else :
+        return JsonResponse ({'bool':False})
+        
