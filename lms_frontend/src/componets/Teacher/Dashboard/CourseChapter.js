167bb5ec3784d8ca5196a160a7af38ae048cb945
adilyb <muhammedadilyb@gmail.com>
2024-05-05 20:16:12 +0530

    delete chapter data

--- lms_frontend/src/componets/Teacher/Dashboard/CourseChapter.js
+++ lms_frontend/src/componets/Teacher/Dashboard/CourseChapter.js
@@ -18,20 +18,49 @@ function CourseChapter() {
                 .then((res) => {
                     setchapterData(res.data);
                     setTotalResult(res.data.length);
-                    console.log(res.data);
+                    // console.log(res.data);
                 })
         } catch (error) {
             console.log(error);
         }
     }, [])
 
-    const handleDeleteClick = () => {
+    const handleDeleteClick = (chapter_id) => {
         Swal.fire({
             title: 'Confirm',
             text: 'Do you want to Delete this Data',
             icon: 'info',
             confirmButtonText: 'Continue',
             showCancelButton: true
+
+        }).then((result)=>{
+            if(result.isConfirmed){
+                try{
+                    axios.delete(baseUrl + '/chapter/' + chapter_id)
+                    .then(()=>{
+                        Swal.fire('success', 'Data has been deleted')
+                        try {
+                            axios.get(baseUrl + '/all-chapter/' + course_id)
+                                .then((res) => {
+                                    setchapterData(res.data);
+                                    setTotalResult(res.data.length);
+                                    // console.log(res.data);
+                                })
+                        } catch (error) {
+                            console.log(error);
+                        }
+                        
+                    })
+                    // window.location.reload();
+                    // .then((res) => {
+                    //     setchapterData(res.data);
+                    //     setTotalResult(res.data.length);
+                        // console.log(res.data);
+                    // })
+                }catch(error){
+                    console.log(error)
+                }
+            }
         })
     }
 
@@ -74,7 +103,7 @@ function CourseChapter() {
 
 
                                             <td>
-                                                <Link onClick={handleDeleteClick} to={"/delete-chapter/" + chapter.id} className="btn btn-sm text-white btn-danger"><i class="bi bi-trash-fill"></i></Link>
+                                                <Link onClick={()=>handleDeleteClick(chapter.id)} className="btn btn-sm text-white btn-danger"><i class="bi bi-trash-fill"></i></Link>
                                                 &nbsp;
                                                 <Link to={"/edit-chapter/" + chapter.id} className="btn btn-sm text-white btn-info" ><i class="bi bi-pencil-square"></i></Link>
                                             </td>
